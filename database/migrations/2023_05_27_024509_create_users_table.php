56065fdc7bcbfd625c253367ead830b249a7656f
root <gustavocamalionti@gmail.com>
2023-05-26 23:52:38 -0300

    :sparkles: feat: initials migrations estructure

--- database/migrations/2023_05_27_024509_create_users_table.php
+++ database/migrations/2023_05_27_024509_create_users_table.php
@@ -13,12 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('name', 255)->notNullable();
+            $table->string('email', 255)->unique();
+            $table->string('document', 14)->unique();
+            $table->unsignedBigInteger('user_types_id');
+            $table->decimal('balance', 10, 2)->default(0);
             $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
+
+            $table->foreign('user_types_id')->references('id')->on('user_types');
         });
     }
 
