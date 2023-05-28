538eb52aaaca98fddb2e44f0cbcc5dca453da77d
root <gustavocamalionti@gmail.com>
2023-05-28 00:57:48 -0300

    :sparkles: feat: Logic of requesting authorization from the external service for transactions

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\TransactionsRequest;
 use Illuminate\Http\Request;
 use App\Services\TransactionService;
+use Illuminate\Database\QueryException;
+use App\Http\Requests\TransactionsRequest;
 
 class TransactionsController extends Controller
 {
@@ -14,8 +15,42 @@ class TransactionsController extends Controller
         $this->transactionService = $transactionService;
     }
 
-    public function makeTransaction(TransactionsRequest $request){
-        dd($request->all());
-    }
+    public function makeTransaction(TransactionsRequest $request)
+    {
+        try {
+            $TransactionAuthorized = $this->transactionService->getAuthorization();
+            if ($TransactionAuthorized == false) {
+                return response()->json([
+                    'msg' => 'Erro',
+                    'data' => [
+                        'message' => 'Não Autorizado'
+                    ]
+                ], 403);
+            };
+            $response = $this->transactionService->addJobTransactionToQueue($request->all());
+            $this->transactionService->sendEmailToPayer($request->all());
+            $this->transactionService->sendEmailToReceived($request->all());
 
+            return response()->json([
+                'msg' => $response
+            ], 200);
+
+        } catch (QueryException $e) {
+            return response()->json([
+                'msg' => 'Erro',
+                'data' => [
+                    'code' => $e->getCode(),
+                    'message' => $e->getMessage()
+                ]
+            ], 500);
+        } catch (\Exception $e) {
+            return response()->json([
+                'msg' => 'Erro',
+                'data' => [
+                    'code' => $e->getCode(),
+                    'message' => $e->getMessage()
+                ]
+            ], 500);
+        }
+    }
 }
