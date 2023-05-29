65c1988174e493aafbce43cdddd1710e1820c6d3
root <gustavocamalionti@gmail.com>
2023-05-28 21:38:46 -0300

    :books: docs: add swagger API DOC in project

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -15,17 +15,52 @@ class TransactionsController extends Controller
         $this->transactionService = $transactionService;
     }
 
+        /**
+     * @OA\Post(
+     *     tags={"/transactions/"},
+     *     path="/make/",
+     *     summary="Get especific user",
+     *     @OA\Parameter(
+     *         description="Description example.",
+     *         in="path",
+     *         name="sender_user_id",
+     *         required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Parameter(
+     *         description="Description example.",
+     *         in="path",
+     *         name="receiver_user_id",
+     *         required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Parameter(
+     *         description="Description example.",
+     *         in="path",
+     *         name="amount",
+     *         required=true,
+     *         @OA\Schema(type="decimal")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="OK"
+     *     ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Error in database or server."
+     *     )
+     * )
+     */
     public function makeTransaction(TransactionsRequest $request)
     {
         try {
-            
+
             $response = $this->transactionService->addTransactionJobToQueue($request->all());
-            
+
 
             return response()->json([
                 'msg' => $response
             ], 200);
-
         } catch (QueryException $e) {
             return response()->json([
                 'msg' => 'Erro',
